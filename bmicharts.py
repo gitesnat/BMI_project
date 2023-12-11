06cb126ba0289ec618aae679500c1919f2efcad5
Michał Janik <100161252+MJanik167@users.noreply.github.com>
2023-12-11 20:55:08 +0100

    plik main i wszystko smiga razem

--- bmicharts.py
+++ bmicharts.py
@@ -9,23 +9,24 @@ daneGotowe = {
   "bmi":[]
 }
 
-dane = open("./dane.txt", "r")  
-pomiary = dane.readlines()
-for item in pomiary:
-  item = item.split()
-  daneGotowe["system"].append(item[0]) 
-  daneGotowe["waga"].append(int(item[1])*(0.45 if item[0]=="I"else 1))
-  daneGotowe["wzrost"].append(float(item[2])*(0.025 if item[0]=="I"else 1))
-  daneGotowe["bmi"].append(float(item[3]))
-dataFrame =  pd.DataFrame.from_dict(data=daneGotowe)
-dataFrame = dataFrame.sort_values(by=["wzrost"], ascending=False)
-print(dataFrame)
+def bmicharts():
+  dane = open("./dane.txt", "r")  
+  pomiary = dane.readlines()
+  for item in pomiary:
+    item = item.split()
+    daneGotowe["system"].append(item[0]) 
+    daneGotowe["waga"].append(int(item[1])*(0.45 if item[0]=="I"else 1))
+    daneGotowe["wzrost"].append(float(item[2])*(0.025 if item[0]=="I"else 1))
+    daneGotowe["bmi"].append(float(item[3]))
+  dataFrame =  pd.DataFrame.from_dict(data=daneGotowe)
+  dataFrame = dataFrame.sort_values(by=["wzrost"], ascending=False)
+  print(dataFrame)
 
-sns.set_theme()
-sns.relplot(
-data=dataFrame,
-x=dataFrame["waga"], y=dataFrame["wzrost"], 
-col="system", hue="bmi",
-)
+  sns.set_theme()
+  sns.relplot(
+  data=dataFrame,
+  x=dataFrame["waga"], y=dataFrame["wzrost"], 
+  col="system", hue="bmi",
+  )
 # sns.relplot(data=dataFrame,
 # x="waga", y="wzrost", values="bmi", kind="line")
